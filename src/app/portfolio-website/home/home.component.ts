d78116e0f36e42a7dab830fce58b5ec7bcbd1b94
Vijaym62 <vijaym6269@gmail.com>
2024-08-31 18:45:55 +0530

    Initial commit

---
+++ src/app/portfolio-website/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component,} from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [NavbarComponent,FooterComponent,MatCardModule],
+  templateUrl: './home.component.html',   
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+KnowMore(){
+
+}
+
+skill:any;
+onKnowMore(skill:any) { 
+console.log("know more",skill)
+}
+
+
+
+}
+
